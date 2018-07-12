3db58764a94c2e5a806ac71c79599a41302b27a7
Jeroen Boersma <jeroen@srcode.nl>
2018-07-12 08:05:34 +0000

    Added rule trait and interface

---
+++ src/Parser/Interfaces/RuleInterface.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: jeroen
+ * Date: 11-7-18
+ * Time: 14:29
+ */
+
+namespace Dutchlabelshop\Parser\Interfaces;
+
+use Dutchlabelshop\Parser\Context;
+
+interface RuleInterface
+{
+
+    public function match(Context $context): bool;
+
+    public function parse(Context $context): bool;
+    public function addRule(RuleInterface $rule);//: void;
+
+}
