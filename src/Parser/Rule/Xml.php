8b03078ee54a766d782ee9d52a09beb71d564425
Jeroen Boersma <jeroen@srcode.nl>
2018-08-11 22:08:17 +0000

    Added SimpleXML parser
    + simple xml parser

---
+++ src/Parser/Rule/Xml.php
@@ -0,0 +1,83 @@
+<?php declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: jeroen
+ * Date: 11-8-18
+ * Time: 22:16
+ */
+
+namespace Elgentos\Parser\Rule;
+
+use Elgentos\Parser\Context;
+use Elgentos\Parser\Interfaces\RuleInterface;
+
+class Xml implements RuleInterface
+{
+
+    public function parse(Context $context): bool
+    {
+        $current = &$context->getCurrent();
+        if (! is_string($current)) {
+            return false;
+        }
+
+        $current = $this->fromXml($current);
+        return true;
+    }
+
+    private function fromXml(string $current): array
+    {
+        $xml = simplexml_load_string($current, \SimpleXMLIterator::class);
+        return $this->walkXml($xml);
+    }
+
+    private function walkXml(\SimpleXMLIterator $parent)
+    {
+        $parent->rewind();
+
+        $attributes = $parent->attributes();
+        $hasAttributes = $attributes->count() > 0;
+        $hasChildren = $parent->count() > 0;
+
+        if (! $hasAttributes && ! $hasChildren) {
+            return (string)$parent;
+        }
+
+        $result = [];
+        if ($hasAttributes) {
+            $result['@attributes'] = ((array)$attributes)['@attributes'];
+        }
+
+        if (! $hasChildren) {
+            $result['@value'] = (string)$parent;
+            return $result;
+        }
+
+        $children = [];
+        for (; $parent->valid(); $parent->next()) {
+
+            $key = $parent->key();
+            /** @var \SimpleXMLIterator $current */
+            $current = $parent->current();
+
+                if (! isset($children[$key])) {
+                $children[$key] = [];
+            }
+
+            $children[$key][] = $this->walkXml($current);
+        }
+
+        $children = array_map(function(&$child) {
+            if (count($child) > 1) {
+                return $child;
+            }
+            return $child[0];
+        }, $children);
+
+        $result = array_merge($result, $children);
+
+        return $result;
+    }
+
+
+}
---
+++ tests/Parser/Rule/XmlTest.php
@@ -0,0 +1,98 @@
+<?php declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: jeroen
+ * Date: 11-8-18
+ * Time: 22:15
+ */
+
+namespace Elgentos\Parser\Rule;
+
+use Elgentos\Parser\Context;
+use Elgentos\Parser\Interfaces\RuleInterface;
+use PHPUnit\Framework\TestCase;
+
+class XmlTest extends TestCase
+{
+
+    public function testImplementsRuleInterface()
+    {
+        $xml = new Xml;
+
+        $this->assertInstanceOf(RuleInterface::class, $xml);
+    }
+
+    public function testInvalidType()
+    {
+        $data = [
+                true
+        ];
+
+        $result = [
+                true
+        ];
+
+        $context = new Context($data);
+
+        $xml = new Xml;
+
+        $this->assertFalse($xml->parse($context));
+        $this->assertSame($data, $result);
+    }
+
+    public function testParse()
+    {
+        $data = [
+                <<<XML
+<xml>
+    <person state="state1">
+        <name>Person1</name>
+        <age>10</age>
+    </person>
+    <otherperson></otherperson>
+    <person>
+        <age>20</age>
+        <name state="state2">Person2</name>
+        <age>30</age>
+    </person>
+</xml>
+XML
+        ];
+
+        $result = [
+            [
+                'person' => [
+                    [
+                        '@attributes' => [
+                            'state' => 'state1'
+                        ],
+                        'name' => 'Person1',
+                        'age' => '10'
+                    ],
+                    [
+                        'age' => [
+                                '20',
+                                '30'
+                        ],
+                        'name' => [
+                            '@attributes' => [
+                                'state' => 'state2'
+                            ],
+                            '@value' => 'Person2'
+                        ],
+                    ],
+                ],
+                'otherperson' => '',
+            ]
+        ];
+
+        $context = new Context($data);
+
+        $xml = new Xml;
+
+        $this->assertTrue($xml->parse($context));
+        $this->assertSame($result, $data);
+    }
+
+
+}
