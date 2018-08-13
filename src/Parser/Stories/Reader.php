b5b5f2a6209ee21d992d0bae9e5d4c700927eeb5
Jeroen Boersma <jeroen@srcode.nl>
2018-08-13 22:08:46 +0000

    Created usefull metrics
    * checked and renamed to usefull metrics

--- src/Parser/Stories/Reader.php
+++ src/Parser/Stories/Reader.php
@@ -48,6 +48,11 @@ class Reader implements StoriesInterface
     const IMPORT = '@import';
     const IMPORT_DIR = '@import-dir';
 
+    /** @var Story */
+    private $importStoryCheckIndex;
+    /** @var Story */
+    private $importStoryNoCheckIndex;
+
     public function __construct(string $rootDir = '.')
     {
         $this->storyMetrics = new StoryMetrics;
@@ -68,7 +73,7 @@ class Reader implements StoriesInterface
     protected function initStory(string $rootDir): Story
     {
         return $this->getMetrics()->createStory(
-                'root',
+                '1-root',
                 $this->filesStory($rootDir),
                 $this->mergeStory(),
                 $this->finalStory()
@@ -94,7 +99,7 @@ class Reader implements StoriesInterface
         return new LoopAll(
                 $this->import($rootDir, '#\.json$#', $checkIndex),
                 $this->getMetrics()->createStory(
-                        '@import::json',
+                        self::IMPORT . '::json' . ($checkIndex ? '+' : '-'),
                         new Json,
                         $checkIndex ? new Rename(self::PREFIX . self::IMPORT) : new NoLogic(true)
                 )
@@ -106,7 +111,7 @@ class Reader implements StoriesInterface
         return new LoopAll(
                 $this->import($rootDir, '#\.txt$#', $checkIndex),
                 $this->getMetrics()->createStory(
-                        '@import::text',
+                        self::IMPORT . '::text' . ($checkIndex ? '+' : '-'),
                         new Trim,
                         $checkIndex ? new Rename(self::PREFIX . self::IMPORT) : new NoLogic(true)
                 )
@@ -118,7 +123,7 @@ class Reader implements StoriesInterface
         return new LoopAll(
                 $this->import($rootDir, '#\.ya?ml$#', $checkIndex),
                 $this->getMetrics()->createStory(
-                        '@import::yaml',
+                        self::IMPORT . '::yaml' . ($checkIndex ? '+' : '-'),
                         new Yaml,
                         $checkIndex ? new Rename(self::PREFIX . self::IMPORT) : new NoLogic(true)
                 )
@@ -130,7 +135,7 @@ class Reader implements StoriesInterface
         return new LoopAll(
                 $this->import($rootDir, '#\.csv$#', $checkIndex),
                 $this->getMetrics()->createStory(
-                        '@import::csv',
+                        self::IMPORT . '::csv' . ($checkIndex ? '+' : '-'),
                         new Trim,
                         new Explode,
                         new Csv(true),
@@ -144,7 +149,7 @@ class Reader implements StoriesInterface
         return new LoopAll(
                 $this->import($rootDir, '#\.xml$#', $checkIndex),
                 $this->getMetrics()->createStory(
-                        '@import::csv',
+                        self::IMPORT . '::xml' . ($checkIndex ? '+' : '-'),
                         new Trim,
                         new Xml,
                         $checkIndex ? new Rename(self::PREFIX . self::IMPORT) : new NoLogic(true)
@@ -154,22 +159,33 @@ class Reader implements StoriesInterface
 
     protected function importStory(string $rootDir, bool $checkIndex): RuleInterface
     {
-        return $this->getMetrics()->createStory(
-                '@import',
-                new LoopAny(
+        if ($checkIndex && null !== $this->importStoryCheckIndex) {
+            return $this->importStoryCheckIndex;
+        } elseif (!$checkIndex && null !== $this->importStoryNoCheckIndex) {
+            return $this->importStoryNoCheckIndex;
+        }
+
+        $importStory = $this->getMetrics()
+                ->createStory(
+                        self::IMPORT . ($checkIndex ? '+' : '-'),
                         $this->fromText($rootDir, $checkIndex)
                         , $this->fromJson($rootDir, $checkIndex)
                         , $this->fromXml($rootDir, $checkIndex)
                         , $this->fromYaml($rootDir, $checkIndex)
                         , $this->fromCsv($rootDir, $checkIndex)
-                )
-        );
+                );
+
+        $checkIndex
+                && ($this->importStoryCheckIndex = $importStory)
+                || ($this->importStoryNoCheckIndex = $importStory);
+
+        return $importStory;
     }
 
     protected function iterateStory(string $rootDir): RuleInterface
     {
         return $this->getMetrics()->createStory(
-                'root-files-iterate',
+                '1.1.1-iterate',
                 new Iterate(
                         new LoopAny(
                             $this->importStory($rootDir, true),
@@ -186,56 +202,53 @@ class Reader implements StoriesInterface
         $csvIsBefore = new Match(new IsRegExp('#\.csv$#'));
         $csvIsAfter = new Match(new IsArray);
 
-        return $this->getMetrics()->createStory(
-                '@glob',
-                new LoopAll(
-                        new Match(
-                                new MatchAll(
-                                        new IsString,
-                                        new IsExact(self::IMPORT_DIR, 'getIndex')
-                                )
-                        ),
-                        $this->getMetrics()->createStory(
-                                '@glob-read',
-                                new Callback(function() use (&$isCsv) {
-                                    $isCsv = true;
-                                    return true;
-                                }),
-                                new Glob($rootDir),
-                                new Iterate(
-                                        $this->getMetrics()->createStory(
-                                                '@glob-import',
-                                                new Callback(function(Context $context) use (&$isCsv, $csvIsBefore) {
-                                                    if (! $isCsv) {
-                                                        return false;
-                                                    }
+        return new LoopAll(
+                new Match(
+                        new MatchAll(
+                                new IsString,
+                                new IsExact(self::IMPORT_DIR, 'getIndex')
+                        )
+                ),
+                $this->getMetrics()->createStory(
+                        self::IMPORT_DIR,
+                        new Callback(function() use (&$isCsv) {
+                            $isCsv = true;
+                            return true;
+                        }),
+                        new Glob($rootDir),
+                        new Iterate(
+                                $this->getMetrics()->createStory(
+                                        self::IMPORT_DIR . '::iterate',
+                                        new Callback(function(Context $context) use (&$isCsv, $csvIsBefore) {
+                                            if (! $isCsv) {
+                                                return false;
+                                            }
 
-                                                    return $isCsv = $csvIsBefore->parse($context);
-                                                }),
-                                                $this->importStory($rootDir, false),
-                                                new Callback(function(Context $context) use (&$isCsv, $csvIsAfter) {
-                                                    if (! $isCsv) {
-                                                        return false;
-                                                    }
+                                            return $isCsv = $csvIsBefore->parse($context);
+                                        }),
+                                        $this->importStory($rootDir, false),
+                                        new Callback(function(Context $context) use (&$isCsv, $csvIsAfter) {
+                                            if (! $isCsv) {
+                                                return false;
+                                            }
 
-                                                    return $isCsv = $csvIsAfter->parse($context);
-                                                })
-                                        ),
-                                        false
+                                            return $isCsv = $csvIsAfter->parse($context);
+                                        })
                                 ),
-                                new Callback(function(Context $context) use (&$isCsv) {
-                                    if (! $isCsv) {
-                                        return false;
-                                    }
-
-                                    $current = &$context->getCurrent();
-                                    $current = [array_merge(...$current)];
-
-                                    return true;
-                                })
+                                false
                         ),
-                        new Rename(self::PREFIX . self::IMPORT_DIR)
-                )
+                        new Callback(function(Context $context) use (&$isCsv) {
+                            if (! $isCsv) {
+                                return false;
+                            }
+
+                            $current = &$context->getCurrent();
+                            $current = [array_merge(...$current)];
+
+                            return true;
+                        })
+                ),
+                new Rename(self::PREFIX . self::IMPORT_DIR)
         );
     }
 
@@ -243,7 +256,7 @@ class Reader implements StoriesInterface
     {
         return new Changed(
                 $this->getMetrics()->createStory(
-                        'root-files'
+                        '1.1-files'
                         , $this->importStory($rootDir, true)
                         , $this->iterateStory($rootDir)
                 )
@@ -254,7 +267,7 @@ class Reader implements StoriesInterface
     {
         return new Changed(
                 $this->getMetrics()->createStory(
-                        'root-merge',
+                        '1.2-merge',
                         new Iterate(
                                 new LoopAny(
                                         $this->mergeImport(),
@@ -274,7 +287,11 @@ class Reader implements StoriesInterface
                         new IsArray,
                         new IsExact(self::PREFIX . self::IMPORT, 'getIndex')
                 ),
-                $this->getMetrics()->createStory('root-merge-import', new MergeDown(true))
+                $this->getMetrics()
+                        ->createStory(
+                                '1.2.1-import',
+                                new MergeDown(true)
+                        )
         );
     }
 
@@ -285,7 +302,11 @@ class Reader implements StoriesInterface
                         new IsString,
                         new IsExact(self::PREFIX . self::IMPORT, 'getIndex')
                 ),
-                $this->getMetrics()->createStory('root-merge-text', new Rename('text'))
+                $this->getMetrics()
+                        ->createStory(
+                                '1.2.2-text',
+                                new Rename('text')
+                        )
         );
     }
 
@@ -296,7 +317,8 @@ class Reader implements StoriesInterface
                         new IsArray,
                         new IsExact(self::PREFIX . self::IMPORT_DIR, 'getIndex')
                 ),
-                $this->getMetrics()->createStory('root-merge-glob',
+                $this->getMetrics()->createStory(
+                        '1.2.3-glob',
                         new Iterate(
                                 new Match(
                                         new IsArray,
@@ -311,7 +333,11 @@ class Reader implements StoriesInterface
 
     protected function finalStory(): RuleInterface
     {
-        return $this->getMetrics()->createStory('root-final', new MergeDown(false));
+        return $this->getMetrics()
+                ->createStory(
+                        '1.3-final',
+                        new MergeDown(false)
+                );
     }
 
 }
