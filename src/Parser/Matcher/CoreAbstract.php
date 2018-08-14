338031527d2a8f2d07a95cddbf41db8441324694
Jeroen Boersma <jeroen@srcode.nl>
2018-08-14 10:02:40 +0000

    Adde core abstract for core functions

---
+++ src/Parser/Matcher/CoreAbstract.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: jeroen
+ * Date: 14-8-18
+ * Time: 11:28
+ */
+
+namespace Elgentos\Parser\Matcher;
+
+use Elgentos\Parser\Context;
+use Elgentos\Parser\Interfaces\MatcherInterface;
+
+abstract class CoreAbstract implements MatcherInterface
+{
+
+    /** @var mixed */
+    protected $needle;
+    /** @var string */
+    protected $method;
+    /** @var bool */
+    protected $caseSensitive;
+
+    public function __construct($needle, bool $caseSensitive = true, string $method = 'getCurrent')
+    {
+        $caseSensitive = ! \is_string($needle) ? true : $caseSensitive;
+        $this->caseSensitive = $caseSensitive;
+
+        $this->needle = $caseSensitive ? $needle : \strtolower($needle);
+        $this->method = $method;
+    }
+
+    public function validate(Context $context): bool
+    {
+        $haystack = $context->{$this->method}();
+        if (! $this->caseSensitive) {
+            $haystack = \strtolower($haystack);
+        }
+
+        return $this->execute($haystack);
+    }
+
+    abstract public function execute(&$haystack): bool;
+
+}
---
+++ tests/Parser/Matcher/CoreAbstractTest.php
@@ -0,0 +1,95 @@
+<?php declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: jeroen
+ * Date: 14-8-18
+ * Time: 11:48
+ */
+
+namespace Elgentos\Parser\Matcher;
+
+use Elgentos\Parser\Context;
+use Elgentos\Parser\Interfaces\MatcherInterface;
+use PHPUnit\Framework\TestCase;
+
+class CoreAbstractTest extends TestCase
+{
+
+    public function testShouldImplementMatcherInterface()
+    {
+        $stringAbstract = $this->getMockBuilder(CoreAbstract::class)
+                ->disableOriginalConstructor()
+                ->getMock();
+
+        $this->assertInstanceOf(MatcherInterface::class, $stringAbstract);
+    }
+
+    public function testOptionalArguments()
+    {
+        $stringAbstract = $this->getMockBuilder(CoreAbstract::class)
+                ->setConstructorArgs([''])
+                ->getMock();
+        $this->assertInstanceOf(MatcherInterface::class, $stringAbstract);
+
+    }
+
+    public function testValidate()
+    {
+        $stringAbstract = $this->getMockBuilder(CoreAbstract::class)
+                ->setMethods(['execute'])
+                ->setConstructorArgs(['test'])
+                ->getMock();
+
+        $stringAbstract->expects($this->once())
+                ->method('execute')
+                ->with('testing');
+
+        $root = ['testing'];
+        $context = new Context($root);
+
+        $stringAbstract->validate($context);
+
+    }
+
+    public function testCaseInSensitive()
+    {
+        $stringAbstract = $this->getMockBuilder(CoreAbstract::class)
+                ->setMethods(['execute'])
+                ->setConstructorArgs(['Test', false])
+                ->getMock();
+
+        $reflectionValue = new \ReflectionProperty($stringAbstract, 'needle');
+        $reflectionValue->setAccessible(true);
+
+        $this->assertSame('test', $reflectionValue->getValue($stringAbstract));
+
+        $stringAbstract->expects($this->once())
+                ->method('execute')
+                ->with('testing');
+
+        $root = ['tesTing'];
+        $context = new Context($root);
+
+        $stringAbstract->validate($context);
+    }
+
+    public function testGetIndex()
+    {
+        $stringAbstract = $this->getMockBuilder(CoreAbstract::class)
+                ->setMethods(['execute'])
+                ->setConstructorArgs(['Test', false, 'getIndex'])
+                ->getMock();
+
+        $stringAbstract->expects($this->once())
+                ->method('execute')
+                ->with('testing');
+
+        $root = [
+                'tesTing' => 'answer'
+        ];
+        $context = new Context($root);
+
+        $stringAbstract->validate($context);
+    }
+
+}
