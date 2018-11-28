95d7ae9c549d79961cfb16fafc7bab8f2bd22630
Jeroen Boersma <jeroen@elgentos.nl>
2018-11-28 16:37:28 +0000

    Added Append Rule for flat merging

---
+++ src/Parser/Rule/Append.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: jeroen
+ * Date: 2-11-18
+ * Time: 11:45
+ */
+
+namespace Elgentos\Parser\Rule;
+
+use Elgentos\Parser\Context;
+use Elgentos\Parser\Interfaces\RuleInterface;
+
+class Append implements RuleInterface
+{
+
+    public function parse(Context $context): bool
+    {
+        $root = &$context->getRoot();
+        $index = $context->getIndex();
+        $current = $context->getCurrent();
+
+        unset($root[$index]);
+
+        $root = $this->merge($root, $current);
+
+        return true;
+    }
+
+    protected function merge(array $array1, array $array2): array
+    {
+        return array_merge($array1, $array2);
+    }
+
+}
---
+++ tests/Parser/Rule/AppendTest.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: jeroen
+ * Date: 2-11-18
+ * Time: 11:40
+ */
+
+namespace Elgentos\Parser\Rule;
+
+use Elgentos\Parser\Context;
+use PHPUnit\Framework\TestCase;
+
+class AppendTest extends TestCase
+{
+
+    /** @var Context */
+    private $context;
+
+    public function setUp()
+    {
+        $root = [
+                1, 2, 3,
+                "append" => [4, 5, 6]
+        ];
+        $this->context = new Context($root);
+    }
+
+
+    public function testParse()
+    {
+        $context = $this->context;
+
+        $context->setIndex('append');
+
+        $rule = new Append;
+        $this->assertTrue($rule->parse($context));
+        $this->assertSame([1,2,3,4,5,6], $context->getRoot());
+    }
+
+}
