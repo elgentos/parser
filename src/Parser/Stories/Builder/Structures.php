04f69fcfab0b94133fdaa6395c1735201f3ed491
Jeroen Boersma <jeroen@elgentos.nl>
2018-12-17 22:15:58 +0000

    Added Structures to stack
    + build a structure which can convert into objects

---
+++ src/Parser/Stories/Builder/Structures.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeroen
+ * Date: 17-12-18
+ * Time: 15:15
+ */
+
+namespace Elgentos\Parser\Stories\Builder;
+
+
+use Elgentos\Parser\Context;
+use Elgentos\Parser\Exceptions\GeneralException;
+use Elgentos\Parser\Interfaces\StoriesInterface;
+use Elgentos\Parser\Matcher\Exact;
+use Elgentos\Parser\Rule\Callback;
+use Elgentos\Parser\Rule\Factory;
+use Elgentos\Parser\Rule\Iterate;
+use Elgentos\Parser\Rule\Match;
+use Elgentos\Parser\Rule\MergeDown;
+use Elgentos\Parser\Story;
+use Elgentos\Parser\StoryMetrics;
+use mysql_xdevapi\Exception;
+
+class Structures implements StoriesInterface
+{
+
+    /** @var array */
+    private $factories;
+    /** @var StoryMetrics */
+    private $storyMetrics;
+    /** @var Story */
+    private $story;
+
+    /**
+     * Objects constructor.
+     *
+     * @param array $factories
+     */
+    public function __construct(array $factories = [])
+    {
+        $this->factories = array_map(function(Factory $factory) {
+            return $factory;
+        }, $factories);
+
+        $this->storyMetrics = new StoryMetrics;
+        $this->story = $this->initStory();
+    }
+
+    /**
+     * Get Story
+     *
+     * @return Story
+     */
+    public function getStory(): Story
+    {
+        return $this->story;
+    }
+
+    /**
+     * Get Story metrics
+     *
+     * @return StoryMetrics
+     */
+    public function getMetrics(): StoryMetrics
+    {
+        return $this->storyMetrics;
+    }
+
+    protected function initStory(): Story
+    {
+        return $this->storyMetrics->createStory(
+                '0-root',
+                $this->objectStory()
+        );
+
+    }
+
+    protected function objectStory(): Story
+    {
+        return $this->storyMetrics->createStory(
+                '1-builder',
+                new Callback(function(Context $context) {
+                    return $this->objectCallback($context);
+                })
+        );
+    }
+
+    protected function objectCallback(Context $context): bool
+    {
+        $current = &$context->getCurrent();
+
+        $parser = $this->getFactory($current['factory']);
+
+        if (isset($current['children'])) {
+
+            $childContext = new Context($current['children']);
+            $stories = array_map(function($index) use ($childContext) {
+
+                $childContext->setIndex($index);
+                $this->objectCallback($childContext);
+
+                return new Iterate(
+                    new Match(
+                        new Exact($index, 'getIndex'),
+                        $childContext->getCurrent()
+                    ),
+                    false
+                );
+
+            }, array_keys($current['children']));
+
+            $stories[] = $parser;
+            $parser = new Story('', ...$stories);
+        }
+
+        if (isset($current['multiple'])) {
+            $parser = new Iterate($parser, false);
+        }
+
+        // Update current
+        $current = $parser;
+
+        return true;
+    }
+
+    protected function getFactory($factory)
+    {
+        if (! is_array($factory)) {
+            return $this->getDefinedFactory($factory);
+        }
+
+        $class = $factory['class'];
+        $arguments = $factory['arguments'] ?? null;
+        $setters = $factory['setters'] ?? null;
+
+        return new Factory($class, $arguments, $setters);
+    }
+
+    protected function getDefinedFactory($factory)
+    {
+        if (! isset($this->factories[$factory])) {
+            throw new GeneralException("Class `{$factory}` is not defined.");
+        }
+
+        return $this->factories[$factory];
+    }
+
+}
---
+++ tests/Parser/Stories/Builder/StructuresTest.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeroen
+ * Date: 17-12-18
+ * Time: 21:33
+ */
+
+namespace Elgentos\Parser\Stories\Builder;
+
+use Elgentos\Parser\Context;
+use Elgentos\Parser\Exceptions\GeneralException;
+use Elgentos\Parser\Rule\Factory;
+use Elgentos\Parser\Rule\Iterate;
+use Elgentos\Parser\Rule\NoLogic;
+use Elgentos\Parser\Story;
+use Elgentos\Parser\StoryMetrics;
+use PHPUnit\Framework\TestCase;
+
+class StructuresTest extends TestCase
+{
+
+    public function testGetMetrics()
+    {
+        $structures = new Structures;
+        $this->assertInstanceOf(StoryMetrics::class, $structures->getMetrics());
+    }
+
+    public function testGetStory()
+    {
+        $structures = new Structures;
+        $this->assertInstanceOf(Story::class, $structures->getStory());
+    }
+
+    public function testConstructurWithFactories()
+    {
+        $factories = [
+            'factory1' => new Factory(NoLogic::class, [true]),
+            'factory2' => new Factory(NoLogic::class, [false]),
+            'factory3' => new \stdClass,
+        ];
+
+        $this->expectException(\TypeError::class);
+        new Structures($factories);
+    }
+
+    public function testStoryMetrics()
+    {
+        $structures = new Structures;
+
+        $metrics = $structures->getMetrics()
+                ->getStatistics('%s');
+        $this->assertSame([
+                '0-root',
+                '1-builder'
+        ], $metrics);
+    }
+
+    public function testStructure()
+    {
+        $factories = [
+            'stdClass' => new Factory(\stdClass::class)
+        ];
+
+        $structures = new Structures($factories);
+
+        /** @var Story $template */
+        $template = [
+            /**
+             * factory is a defined class
+             */
+            'factory' => [
+                'class' => Story::class,
+                /**
+                 * default arguments
+                 */
+                'arguments' => [
+                    'name' => 'test-story'
+                ]
+            ],
+            'children' => [
+                'iterate' => [
+                    'factory' => [
+                        'class' => Iterate::class,
+                        'arguments' => [
+                            'recursive' => false,
+                        ]
+                    ],
+                    'multiple' => true,
+                ],
+                /**
+                 * this is a lookup factory defined in factories
+                 */
+                'standard' => [
+                    'factory' => 'stdClass',
+                ]
+            ]
+        ];
+        $content = ['@template' => &$template];
+
+        $context = new Context($content);
+        $structures->getStory()->parse($context);
+
+        $this->assertInstanceOf(Story::class, $template);
+        /**
+         * all childs should be a page before the actual root
+         */
+        $this->assertSame(3, $template->getPages());
+    }
+
+    public function testUndefinedFactory()
+    {
+        $factories = [
+            'stdClass' => new Factory(\stdClass::class)
+        ];
+
+        $this->expectException(GeneralException::class);
+
+        $template = [
+            'factory' => 'stdClass',
+            'children' => [
+                'standard' => [
+                    'factory' => 'non-existent'
+                ]
+            ],
+        ];
+
+        $content = [&$template];
+        $context = new Context($content);
+
+
+        $structures = new Structures($factories);
+
+        $structures->getStory()
+                ->parse($context);
+    }
+
+}
